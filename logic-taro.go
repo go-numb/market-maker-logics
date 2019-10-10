9355cdc58eb8d5f17db59b89d96f1d1b8fa5b9de
numbp <o9o73286699@gmail.com>
2019-10-11 01:18:37 +0900

    logic by taro

---
+++ logic-taro.go
@@ -0,0 +1,33 @@
+package taros
+
+import "fmt"
+
+// Reset is  resets by ticker time.
+// Use executions ticker or executions, and volumes to vwap.
+func (p *Execute) Reset() { // 乖離加速度Logic@taro
+	mean := stat.Mean(p.Prices, p.Sizes) // 直近n秒SMA
+	l := len(p.PricesPast)
+	p.PricesPast = append(p.PricesPast, p.Prices...) // 過去に現在を追加
+	p.SizesPast = append(p.SizesPast, p.Sizes...)    // 過去に現在を追加
+	meanLast := stat.Mean(p.PricesPast, p.SizesPast) // n+x足SMA
+	if 0 < l {                                       // 不要部分を削除
+		p.PricesPast = p.PricesPast[l:]
+		p.SizesPast = p.SizesPast[l:]
+	}
+
+	sma := ((mean - meanLast) + mean) / meanLast
+
+	askPrice := mean * (sma + 0.00026)
+	bidPrice := mean * (sma - 0.00026)
+	diffBuy := askPrice - mean
+	diffSell := mean - bidPrice
+	if diffBuy < diffSell {
+		p.IsBuyByAcceleration = false
+	} else if diffBuy > diffSell {
+		p.IsBuyByAcceleration = true
+	}
+
+	p.PriceDivergense = mean
+	// fmt.Printf("%t\n", p.IsBuyByAcceleration)
+	// fmt.Printf("---------------- %.f (%.f/%.f/%f) %.f ++ %.f  (%.f)\n", p.Price, mean, meanLast, sma, diffBuy, diffSell, bidPrice-askPrice)
+}
